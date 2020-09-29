baf221b956f0c6ca1fc43ecd27a01a01ed46fecd
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-09-29 15:53:39 +0200

    ExA6 fixed for personNumber

--- ExA6/src/be/abis/ExA6/test/ExA6.java
+++ ExA6/src/be/abis/ExA6/test/ExA6.java
@@ -1,5 +1,10 @@
 package be.abis.ExA6.test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 import be.abis.ExA6.model.Address;
@@ -8,19 +13,29 @@ import be.abis.ExA6.model.Person;
 
 public class ExA6 {
 
-	public static void main(String[] args) {
 		
+	public static void main(String[] args) throws IOException {
+		
+			
 		Address address = new Address("Diesteweg", "14/B", "3000", "Leuven", "Belgie", "BE");
 				
 		Company company = new Company("ABIS", address);
 		
 		Person person  = new Person(1, "Ann", "Smits", LocalDate.of(1985, 7 , 23), company ) ;
 		
-		Person person1 = new Person(1, "John", "Doe", LocalDate.of(1966, 7 , 23)) ;
+		Person person1 = new Person(2, "John", "Doe", LocalDate.of(1966, 7 , 23)) ;
 		
 		System.out.println(person);
 		
 		System.out.println(person1);
+	
+		BufferedWriter bw = Files.newBufferedWriter(Paths.get("personinfo.txt"));
+			
+		bw.write(person + "\n");
+		bw.write(person1 + "\n");
+		bw.close();
+			
+		
 		
 	}
 }
