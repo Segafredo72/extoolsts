68705645cc91b85efa78fbe34ce90ae24bb12119
Siegfried Smeets <siegfried.smeets@bnpparibasfortis.com>
2020-09-29 12:18:27 +0200

    Commit ExF4

---
+++ CourseAdministration/src/be/abis/courseadmin/model/Instructor.java
@@ -0,0 +1,111 @@
+package be.abis.courseadmin.model;
+
+import java.util.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileWriter; 
+import java.io.IOException;
+import java.io.File;
+
+public class Instructor {
+	
+    private String firstName;
+	private String lastName;
+	private int currentAge;
+	private int startAge;
+	private double salary;
+	/*
+	public Instructor(String firstName, String lastName, int currentAge, int startAge, double salary){
+		this.firstName 	= firstName;
+		this.lastName 	= lastName;
+		this.currentAge = currentAge;
+		this.startAge 	= startAge;
+		this.salary 	= salary;
+    }
+	*/
+	
+	public Instructor(){
+		this.firstName 	= "";
+		this.lastName 	= "";
+		this.currentAge = 0;
+		this.startAge 	= 0;
+		this.salary 	= 0;
+    }	
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	
+	public int getCurrentAge(){
+		return currentAge;
+	}
+	
+	public void setCurrentAge(int currentAge){
+		this.currentAge = currentAge;
+	}
+		
+	public int getStartAge(){
+		return startAge;
+	}
+			
+	public void setStartAge(int startAge){
+		this.startAge = startAge;
+	}
+		
+	public double getSalary(){
+		return salary;
+	}
+	
+	public void setSalary(double salary){
+		this.salary = salary;
+	}
+	
+	public void printSalaryHistory(){
+	
+	double increasedSalary = 0;	
+	int stopAge = 0;	
+	int nextAge = 0;
+		
+	try{
+		
+	FileWriter myWriter = new FileWriter("salaryhistory.txt");
+	
+	stopAge = startAge + 35;
+	increasedSalary = salary;
+	nextAge = startAge;
+	
+	do{
+	
+	myWriter.write("Salary of "+ firstName + " at " + nextAge + " is " + increasedSalary +"\n" );
+	
+	
+    nextAge +=5;
+	increasedSalary *= 1.03;
+    
+	} while(nextAge <= stopAge && nextAge <= currentAge);
+	
+	if (nextAge >= stopAge) {
+		myWriter.write("Maximum salary reached \n" );
+	}
+	 
+	myWriter.close();
+	
+	}catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+}
+
+}
---
+++ CourseAdministration/src/be/abis/courseadmin/test/ExF4.java
@@ -0,0 +1,141 @@
+package be.abis.courseadmin.test;
+
+import java.util.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileWriter; 
+import java.io.IOException;
+import java.io.File;
+import be.abis.courseadmin.model.*;
+
+public class ExF4{
+
+public static String getName(String firstLast){
+	String name;
+	Scanner readInput = new Scanner(System.in);
+	System.out.println("Please enter instructor's "+firstLast+" name :");
+	name = readInput.nextLine(); 
+	return name;
+}
+
+public static int getAge(String whatAge){
+	Scanner readInput = new Scanner(System.in);
+	boolean continueInput = true;
+	int age	= 0;
+	do {
+	try{
+		System.out.println("Please input instructor age "+whatAge+" :");
+		age = readInput.nextInt(); 
+		continueInput = false;	
+	}catch (InputMismatchException ex) {
+		System.out.println("Please enter a valid age :");
+		readInput.nextLine(); 
+	}
+	} while (continueInput);
+	return age;
+}
+
+public static double getSalary(){
+	Scanner readInput = new Scanner(System.in);
+	boolean continueInput = true;
+	double salary = 0;
+	do{
+	try{
+		System.out.println("Please enter starting salary:");
+		salary = readInput.nextDouble();
+		continueInput = false;	
+	}catch (InputMismatchException ex) {
+		System.out.println("Please enter a valid salary :");
+		readInput.nextLine(); 
+	}
+	}while(continueInput);
+	return salary;
+}
+
+
+public static void createFile (){
+     try {
+      File myObj = new File("salaryhistory.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+	  
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+
+
+
+
+public static void main(String[] args){
+
+
+	createFile();
+		
+	
+	Instructor instructor = new Instructor();
+	Course co1 = new Course();
+	
+	co1.setTitle("Java"); 
+	co1.setNumberOfDays(2);
+	co1.setPricePerDay(300.0);
+	co1.setPriorKnowledgeRequired(false);
+	
+	instructor.setFirstName("Sandy");
+	instructor.setLastName("Schillebeeckx");
+	instructor.setCurrentAge(35);
+    instructor.setStartAge(25);
+	instructor.setSalary(35000.00);
+	
+	co1.addInstructor(instructor);
+
+	Instructor instructor2 = new Instructor();
+	instructor2.setFirstName("Chris");
+	instructor2.setLastName("Froome");
+	instructor2.setCurrentAge(45);
+    instructor2.setStartAge(27);
+	instructor2.setSalary(45000.00);
+	
+	co1.addInstructor(instructor2);
+
+  
+	Course co2 = new Course();
+	
+	co2.setTitle("Python"); 
+	co2.setNumberOfDays(3);
+	co2.setPricePerDay(300.0);
+	co2.setPriorKnowledgeRequired(true);
+	
+	Instructor instructor3 = new Instructor();
+	instructor3.setFirstName("Chris");
+	instructor3.setLastName("Rea");
+	instructor3.setCurrentAge(45);
+    instructor3.setStartAge(27);
+	instructor3.setSalary(45000.00);
+	
+	co2.addInstructor(instructor3);
+		
+	Course[] courses = { co1, co2 };
+
+	try {
+		for (Course c : courses) {
+			System.out.println(c.getTitle() + " costs " + c.calculatePrice());
+			c.printInfo();
+		}
+	}
+
+	catch (IOException e) {
+		System.out.println(e.getMessage());
+	}
+
+	
+		
+	instructor.printSalaryHistory();
+	}
+
+	
+}
